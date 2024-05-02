547b239b3e4f4d0f9be852f6b8101ceead9f5546
Takam Alex Christian <takamalexchristian@gmail.com>
2024-05-02 10:34:01 -0700

    homepage implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
-import Image from "next/image";
+
+import { Button, Link } from '@nextui-org/react'
 
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
@@ -10,8 +11,16 @@ import ImageCapture from "@/components/ImageCapture";
 export default function Home() {
   return (
     <main className="flex flex-col p-8 items-center">
-      <div className="w-[516px]">
-        <ImageCapture />
+      <div className='flex flex-col gap-4'>
+
+        <div className='flex flex-row justify-between p-2'>
+          <h1 className='text-2xl font-black'>FacialAuth</h1>
+        </div>
+        <div className='flex flex-col gap-2'>
+          <Button as={Link} href={"/login"} color='primary'>Login using facialAuth</Button>
+          <Button as={Link} href={"/signup"}>Signup using facialAuth</Button>
+        </div>
+
       </div>
 
     </main>
